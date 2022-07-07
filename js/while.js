43aed353bd8fe1724b05933042fae09b993bada6
ivenckunas <ignas.venckunas@gmail.com>
2022-07-07 16:01:26 +0300

    while

---
+++ js/while.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/* 
+while(salyga){
+    // vygdom koda tol kol salyga === true
+    // turi buti kazkas kas pakeis salyga i false
+}
+*/
+
+let i = 1;
+// i++ = i + 1
+// salyga i <= 10
+while (i <= 10) {
+  console.log(`ciklas ${i}`);
+  // kintamojo didinimas link isejimo salygos
+  i++;
+}
+
+i = 10;
+
+while (i >= -5) {
+  console.log(`ciklas ${i}`);
+  // kintamojo didinimas link isejimo salygos
+  i--;
+}
